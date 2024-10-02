8e958ae99e5ea00cbb6b4d30fc39f2dc3a1e9590
Guylène D <165901249+guylened@users.noreply.github.com>
2024-10-02 09:41:07 +0200

    chemin index + js

--- FrontEnd/assets/JS/import-works.js
+++ FrontEnd/assets/JS/import-works.js
@@ -1,9 +1,22 @@
-const donnees = await fetch("http://localhost:5678/api/works")
+const responseWorks =  fetch("http://localhost:5678/api/works").then(responseWorks => responseWorks.json());
+/*const donneesWorks = responseWorks.json();*/
+
+const responseCategories =  fetch("http://localhost:5678/api/Categories").then(responseCategories => responseCategories.json());
+/*const donneesCategories = responseCategories.json();*/
 
 const sectionGallery = document.querySelector(".gallery");
 
-const image = document.createElement("img")
-const legende = document.createElement("figcaption")
+
+let image = document.createElement("img");
+for (let i = 0; i < responseWorks.length; i++) {
+    image.src = `${responseWorks[i].imageUrl}`
+};
 
 sectionGallery.appendChild(image);
+
+let legende = document.createElement("figcaption");
+for (let i = 0; i < responseWorks.length; i++) {
+    legende.innerHTML = `${responseWorks[i].title}`
+};
+
 sectionGallery.appendChild(legende);
